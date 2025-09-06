afe53365f7d959894f81386fc5f71d6441f491ae
alexanderviolet <alexander.violet@tufts.edu>
2025-09-06 15:30:13 -0400

    tried doing some stuff on my own to figure out hanson but proved fruitless

--- rawness/rawness.c
+++ rawness/rawness.c
@@ -3,11 +3,28 @@
  * September 5, 2025
  * CS40: Lab 1
  */
-#include <stdio.h>
-#include <stdlib.h>
-#include <pnmrdr.h>
+#include "rawness.h"
+
+// #define T Pnmrdr_T
+
+// void tryOpeningFile(int argc, char *argv[]);
 
 int main(int argc, char *argv[])
+{
+        tryOpeningFile(argc, argv);
+
+        extern T Pnmrdr_new(FILE * fp);
+        extern Pnmrdr_mapdata Pnmrdr_data(T rdr);
+
+        return 0;
+}
+
+/*
+ * tryOpeningFile
+ * for parts 6 - 8 we just wanted to see how to open a file in c before getting
+ * to the pnmrdr stuff.
+ */
+void tryOpeningFile(int argc, char *argv[])
 {
         /* Check to see if you have enough arguments*/
         printf("The number of arguments %d\n", argc);
@@ -35,6 +52,4 @@ int main(int argc, char *argv[])
         }
 
         fclose(fptr);
-
-        return 0;
 }
---
+++ rawness/rawness.h
@@ -0,0 +1,15 @@
+/*
+ * Justin and Alex V
+ * September 5, 2025
+ * CS40: Lab 1
+ */
+#ifndef RAWNESS_H
+#define RAWNESS_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <pnmrdr.h>
+#define T Pnmrdr_T
+
+void tryOpeningFile(int argc, char *argv[]);
+
+#endif
